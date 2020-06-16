1013f91216801a7d26bcb7ab90e1eb973b91d780
Simon Hoyos <shmesa23@gmail.com>
2020-06-16 10:01:58 -0500

    NodeJS unit testing

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,6 @@
 require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const db = require('./src/db');
-const userRouter = require('./src/routes/user');
-const taskRouter = require('./src/routes/task');
-
+const app = require('./src/app');
 const port = process.env.PORT || 8000;
-
-const app = express();
-db();
-
-app.use(express.json());
-app.use(cors());
-app.use(morgan('dev'));
-
-app.use(userRouter);
-app.use('/tasks', taskRouter);
 
 app.listen(port, () =>
   console.log(`Server listening on http://localhost:${port}`)
---
+++ backend/src/app.test.js
@@ -0,0 +1,22 @@
+const req = require('supertest');
+const mongoose = require('mongoose');
+const app = require('./app');
+
+describe('app', () => {
+  beforeEach(async () => {
+    for(let collection in mongoose.connection.collections) {
+      await mongoose.connection.collections[collection].deleteMany({});
+    }
+  });
+
+  afterAll(async () => {
+    await mongoose.disconnect();
+  });
+
+  it('should GET / with success code', async done => {
+    const res = await req(app).get('/');
+    done();
+
+    expect(res.statusCode).toBe(200);
+  });
+});
